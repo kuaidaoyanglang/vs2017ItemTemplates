2b7b3c196695e91a6d3d07d1199b3f14ed61b0f5
kuaidaoyanglang <1042744766@qq.com>
2018-09-05 16:17:06 +0800

    first commit

---
+++ Class/Class.cs
@@ -0,0 +1,26 @@
+/****************************************************************
+ * 作    者：$userdomain  $username
+ * CLR 版本：$clrversion$
+ * 创建时间：$time$
+ * 当前版本：1.0.0.1
+ * 机器名称: $machinename
+ * 
+ * 描述说明：$itemname
+ *
+ * 修改历史：
+ *
+*****************************************************************
+ * Copyright @ $username $year$ All rights reserved
+*****************************************************************/
+using System;
+using System.Collections.Generic;
+$if$ ($targetframeworkversion$ >= 3.5)using System.Linq;
+$endif$using System.Text;
+$if$ ($targetframeworkversion$ >= 4.5)using System.Threading.Tasks;
+$endif$
+namespace $rootnamespace$
+{
+    class $safeitemrootname$
+    {
+    }
+}
