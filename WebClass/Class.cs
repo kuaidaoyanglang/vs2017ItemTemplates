729ba6de6eafef31bceb26675194c0c704de1919
kuaidaoyanglang <1042744766@qq.com>
2018-09-05 16:21:12 +0800

    修改

--- WebClass/Class.cs
+++ WebClass/Class.cs
@@ -1,16 +1,16 @@
 ﻿/****************************************************************
- * 作    者：$userdomain  $username
+ * 作    者：$userdomain$  $username$
  * CLR 版本：$clrversion$
  * 创建时间：$time$
  * 当前版本：1.0.0.1
- * 机器名称: $machinename
+ * 机器名称: $machinename$
  * 
- * 描述说明：$itemname
+ * 描述说明：$itemname$
  *
  * 修改历史：
  *
 *****************************************************************
- * Copyright @ $username $year$ All rights reserved
+ * Copyright @ $username$ $year$ All rights reserved
 *****************************************************************/
 using System;
 using System.Collections.Generic;
